e135318fe0a835fd07d9e0018b6cc9e96b363300
SuperSpawn <thearktitan@gmail.com>
2023-02-24 08:02:44 +0200

    got data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import "./App.css";
+
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    
-  );
+  const [productArray, setProductArray] = useState([]);
+
+  useEffect(() => {
+    fetch("https://63f84b981dc21d5465bc6582.mockapi.io/shoes", {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        // handle error
+        throw new Error("cant connect");
+      })
+      .then((tasks) => {
+        // Do something with the list of tasks
+        setProductArray(tasks)
+      })
+      .catch((error) => {
+        // handle error
+        console.error(error.message);
+      });
+  }, []);
+
+  return <div>Hi</div>;
 }
 
 export default App;
